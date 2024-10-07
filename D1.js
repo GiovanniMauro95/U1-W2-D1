7a278f3af173045cbce85cc46827c576f17019c3
Giovanni Mauro <giovannimauro95@hotmail.it>
2024-10-07 14:45:24 +0200

    Added all answers

--- D1.js
+++ D1.js
@@ -11,38 +11,44 @@ REGOLE
  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+// I principali datatypes sono:
+// Le stringhe: Sono valori che contengono parole
+// I boolean: Sono true e false, indicano la veridicità di un valore
+// I null: Attribuiscono un valore di niente al contenitore
+// Gli undefined: Sono valori ai quali non ne è stato assegnato uno, si possono considerare "errori" o dimenticanze
+// Valore numerico: Sono valori contenenti numeri
 
 /* ESERCIZIO 2
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
-
-/* SCRIVI QUI LA TUA RISPOSTA */
+const myName = "Giovanni"
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
-
-/* SCRIVI QUI LA TUA RISPOSTA */
-
+let number1 = 12
+let number2 = 20
+let result = number1 + number2
+console.log(result)
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let x = 12
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+// myName = "Mauro"
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let subtraction = 4 - x
+console.log(subtraction)
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -50,4 +56,7 @@ REGOLE
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let name1 = "john"
+let name2 = "John"
+let names = name1 === name2
+console.log(names)
